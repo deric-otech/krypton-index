2e9571f207c7940dfa161e9b7ddc8a7c83542a44
Aditya Sharma <79913742+w0lfrm@users.noreply.github.com>
2021-07-02 21:25:16 +0530

    asdasd

--- goindex-acrou/go2index/index.js
+++ goindex-acrou/go2index/index.js
@@ -67,7 +67,7 @@ var authConfig = {
 var themeOptions = {
   cdn: "https://cdn.jsdelivr.net/gh/w0lfrm/krypton-index",
   // 主题版本号
-  version: "2.0.8-darkmode-0.1",
+  version: "dark",
   //可选默认系统语言:en/zh-chs/zh-cht
   languages: "en",
   render: {
